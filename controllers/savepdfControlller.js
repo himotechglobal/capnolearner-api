e49b6d7415996dbae63ab5cfa5439b044e95e295
md nasim <mdnasim4234@gmail.com>
2022-07-21 18:45:56 +0530

    latest api

--- controllers/savepdfControlller.js
+++ controllers/savepdfControlller.js
@@ -1,6 +1,8 @@
 
 const dbConn = require('../dbConnection')
+const { jsPDF } = require("jspdf");
 const md5 = require('md5');
+const fs = require('fs');
 
 
 // exports.getpdf = (req, resp) => {
@@ -222,6 +224,135 @@ exports.livesessionImage = (req, resp) => {
             })
         }
     })
+}
+
+
+exports.livesessionImagedownload = (req, resp) => {
+
+    sessionDate = [];
+    sessionImage = [];
+    clientName = [];
+    trainerName = [];
+  
+
+    console.log(req.params.sessionid)
+    dbConn.query('SELECT * FROM capno_data WHERE sessionid = md5(?) and data_type = ?', [req.params.sessionid, req.params.data_type], (err, result) => {
+        if (err) {
+            resp.status(500).json({
+                success: false,
+                message: 'Somothing went wrong'
+            })
+        }
+        // console.log(result[0].sessionid)
+        if (result[0]) {
+            sessionImage.push(result[0].sessiondata);
+            dbConn.query('SELECT * FROM client_session WHERE md5(id) = ?', [result[0].sessionid], (err, resultCid) => {
+                if (err) {
+                    resp.status(500).json({
+                        success: false,
+                        message: 'Somothing went wrong'
+                    })
+                }
+                
+                if (resultCid[0]) {
+                    sessionDate.push(resultCid[0].name)
+                    dbConn.query('SELECT * FROM capno_users WHERE md5(id) = ?', [resultCid[0].cid], (err, getclientResult) => {
+                        if (err) {
+                            resp.status(500).json({
+                                success: false,
+                                message: 'Somothing went wrong'
+                            })
+                        }
+                        if (getclientResult[0].associated_practioner) {
+
+                            clientName.push(getclientResult[0].firstname + " " + getclientResult[0].lastname)
+                            
+                        
+                            dbConn.query('SELECT * FROM capno_users WHERE md5(id) = ?', [getclientResult[0].associated_practioner], (err, finalResult) => {
+
+                                if (err) {
+                                    resp.status(500).json({
+                                        success: false,
+                                        message: 'Somothing went wrong'
+                                    })
+                                }
+                                if (finalResult.length > 0) {
+                                    
+
+                                    trainerName.push(finalResult[0].firstname + " " + finalResult[0].lastname)
+
+                                    // resp.status(200).json({
+                                    //     success: true,
+                                    //     data: finalResult,
+                                    //     result: result[0].sessiondata,
+                                    //     sessionDate: resultCid[0].name,
+                                    //     firstname: getclientResult[0].firstname,
+                                    //     lastname: getclientResult[0].lastname,
+
+                                    // })
+                                }
+                            })
+                        }
+                    })
+                }
+
+
+            })
+        }
+        else{
+            resp.status(202).json({
+                success: false,
+                message: 'Somothing went wrong'
+            })
+        }
+    })
+
+
+    setTimeout( () => {
+
+        let stringsessionDate = sessionDate.toString();
+        let stringsessionImage = sessionImage.toString();
+        let stringclientName = clientName.toString();
+        let stringtrainerName = trainerName.toString();
+
+        const doc = new jsPDF();
+
+        doc.setTextColor(0, 0, 0);
+        doc.text('Capnolearning Report', 10, 10,
+            {styles:{ fontSize: 20,fontWeight: 'bold'}})
+        doc.setDrawColor(0, 0, 0);
+        doc.line(10, 15, 600, 15);
+        doc.setFontSize(10)
+        
+        doc.text(stringsessionDate ,35,25)
+        doc.text(stringclientName,23,30);
+        doc.text(stringtrainerName,25,35);
+        doc.setFont(undefined, 'bold');
+        doc.text("Session Date:" ,10,25)
+        doc.text("Client:" ,10,30);
+        doc.text("Trainer:",10,35);
+        // doc.setFont(undefined, 'bold')
+        console.log(sessionImage.length)
+        var sessionImagerequiredPages = sessionImage.length;
+        for (var i = 0; i < sessionImagerequiredPages; i++) {
+        doc.addImage(stringsessionImage, 5, 45,200,110);
+        }
+        const data = doc.save()
+
+        const datas =fs.readFileSync('./generated.pdf');
+           resp.contentType("application/pdf");
+           resp.send(datas);
+    
+
+        // let _resp = {
+        //     success: true,
+        //     data: data
+           
+        // }
+
+        // return resp.status(200).json(_resp)
+
+    },5000)
 }
 
 
